2e2210fc0d39787c2fa7d203b5ab2b4542cc480e
Kazuki Tobita <kazukiti201@gmail.com>
2018-12-04 18:30:57 +0900

    Add files

---
+++ app/ResumeManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class ResumeManager
+{
+    /**
+     * 履歴書の元となるJSONを読み込み
+     */
+    public function read()
+    {
+        var_dump('read');
+    }
+
+    /**
+     * 履歴書のPDFを生成
+     */
+    public function generate()
+    {
+        var_dump('generate');
+    }
+}
